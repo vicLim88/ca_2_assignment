79e666cda2c8787245bf630762a354cc608b7890
vicLim88 <vic.lim@icloud.com>
2023-08-03 01:11:18 +0800

    able to extract values from csv file

--- main.py
+++ main.py
@@ -1,8 +1,25 @@
 from services.etl.etl_data_gov_sg import etl_data_gov_sg
+from pymongo.mongo_client import MongoClient
+
+import os
+
+def test():
+    uri = "mongodb+srv://viclim:Tt1o2eSfkIEDUmRt@cluster0.jowdxmj.mongodb.net/?retryWrites=true&w=majority"
+    # Create a new client and connect to the server
+    client = MongoClient(uri, tls=True, tlsAllowInvalidCertificates=True)
+    # Send a ping to confirm a successful connection
+    try:
+        client.admin.command('ping')
+        print("Pinged your deployment. You successfully connected to MongoDB!")
+        print(client.list_database_names())
+    except Exception as e:
+        print(e)
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    # test()
     # Load data into mongodb
     etl = etl_data_gov_sg()
-    etl.extract()
+    etl.extract(source="dataset")
     etl.load()
--- services/etl/etl_data_gov_sg.py
+++ services/etl/etl_data_gov_sg.py
@@ -1,6 +1,8 @@
+import os
 import numpy as np
+import pandas as pd
 
-from typing import List
+from typing import List, Dict
 from requests import session
 from requests_html import HTMLSession
 from abc import ABC
@@ -13,6 +15,9 @@ from propertyUtils.config_parser import read_ini_configuration_from_resource
 class etl_data_gov_sg(etl_Main, ABC):
 
     def __init__(self, environment_name: str = "DEV"):
+        self.dict_dbName_colls = None
+        self.list_of_dataset_folders = None
+        self.curr_dir: str = os.getcwd()
         self.list_of_resource_ids = None
         self.dataset_collections_url = None
         self.mongo_db_dev = DatabaseMongo(environment_name=environment_name)
@@ -20,18 +25,42 @@ class etl_data_gov_sg(etl_Main, ABC):
         self._setup_from_ini_configuration()
 
     # PUBLIC METHOD
-    def extract(self):
-        # Extract list of resource_ids from data.gov.sg
-        self.list_of_resource_ids: List[str] = self._scrap_links_for_resource_ids()
+    def extract(self, source: str = "INTERNET"):
+        if source.upper() == "DATASET":
+            self.dict_dbName_colls = self._fetch_list_of_dataset_folders()
 
-        # Save the list of resource_ids for record keeping
-        self._ini_configuration_update()
+        else:
+            # Extract list of resource_ids from data.gov.sg
+            self.list_of_resource_ids: List[str] = self._scrap_links_for_resource_ids()
 
-    def load(self):
+            # Save the list of resource_ids for record keeping
+            self._ini_configuration_update()
+
+    def load(self) -> None:
         # extract a list of records
-        for resource_id in self.list_of_resource_ids:
-            name, record = self._extract_record(resource_id)
-            self._write_to_mongodb(name, record)
+        if self.dict_dbName_colls is not None:
+            for dbName, colls in self.dict_dbName_colls.items():
+                for coll in colls:
+                    if ".csv" in coll:
+                        csv_file_path = f"{self.curr_dir}/resources/datasets/{dbName}/{coll}"
+                        entries = pd.read_csv(csv_file_path).dropna().to_dict('records')
+                        print(entries)
+                        self._write_to_mongodb(
+                            database_name=dbName,
+                            collection_name=coll,
+                            entries=entries
+                        )
+            return
+
+        if (self.list_of_resource_ids is not None) and (len(self.list_of_resource_ids) > 0):
+            for resource_id in self.list_of_resource_ids:
+                name, record = self._extract_record(resource_id)
+                self._write_to_mongodb(
+                    database_name="COVID-19_Weekly_status",
+                    collection_name=name,
+                    entries=record
+                )
+            return
 
     # PRIVATE METHOD
     def _setup_from_ini_configuration(self):
@@ -49,17 +78,30 @@ class etl_data_gov_sg(etl_Main, ABC):
             value=", ".join(self.list_of_resource_ids)
         )
 
-    def _write_to_mongodb(self, collection_name: str, response_json):
+    def _write_to_mongodb(self, database_name: str, collection_name: str, entries) -> None:
         self.mongo_db_dev.create_multiple_documents(
-            name_of_database="COVID-19_Weekly_Stats",
+            name_of_database=database_name,
             name_of_collection=collection_name.replace(" ", "_").replace(" / ", "_"),
-            entries=response_json
+            entries=entries
         )
+        return
+
+    def _write_to_mongodb_from_datasets(self) -> None:
+        for collection_name in self.list_of_dataset_folders:
+            document_name = f"{self.curr_dir}/"
+            print(collection_name)
+        # self.mongo_db_dev.create_multiple_documents(
+        #     name_of_database="COVID-19_Weekly_Stats",
+        #     name_of_collection=collection_name.replace(" ", "_").replace(" / ", "_"),
+        #     entries=response_json
+        # )
 
     def _extract_record(self, resource_id: str):
         response = HTMLSession().get(self.base_url)
         key: str = response.html.xpath(f".//a[contains(@href, '{resource_id}')]")[0].text
-        value = session().get(
+        value = session.Sess().set(
+
+        ).get(
             url=f"https://data.gov.sg/api/action/datastore_search?resource_id={resource_id}"
         ).json()['result']['records']
         return key, value
@@ -68,5 +110,16 @@ class etl_data_gov_sg(etl_Main, ABC):
         response = HTMLSession().get(self.base_url)
         list_of_ids = [link.split("?")[1].split("&")[0].split("=")[1] for link in response.html.absolute_links if
                        "?resource_id=" in link]
+        assert len(list_of_ids) > 0, "Unable to retrieve list of ids"
         list_of_ids_uniq = np.unique(list_of_ids, axis=0).tolist()
         return list_of_ids_uniq
+
+    def _fetch_list_of_dataset_folders(self):
+        base_path = f"{self.curr_dir}/resources/datasets"
+        dict_of_db_colls = {}
+        for roots in os.walk(base_path):
+            if (roots is not None) and (roots[0] != base_path):
+                db_name = roots[0].split('\\')[-1]
+                collections = roots[2]
+                dict_of_db_colls[db_name] = collections
+        return dict_of_db_colls
